d91042c18fab4e450705c2bab03ca2696dbc8e56
Andrew Seymour <andrew.seymour1@gmail.com>
2022-08-25 16:26:19 -0500

    removed extra server file from notetaker directory

--- Develop/server.js
+++ Develop/server.js
@@ -25,20 +25,3 @@ app.use("/", htmlRoutes);
 app.listen(PORT, () => {
   console.log("API server now on port ${PORT}");
 });
-
-function CareerObjParseFromString(str) {
-  let array = str.split(","); // [0] = name:drew, [1]=job:programmer
-  let obj = {};
-  for (let i = 0; i < array.length; i++) {
-    if (str.substring("name:")) {
-      obj.name = str.split(":"); // [0] =name, [1] drew
-    }
-    if (str.substring("job:")) {
-      obj.job = str.split(":"); // [0] =job, [1] programmer
-    }
-  }
-  return obj;
-}
-
-let request_string = "name:drew,job:programmer"; // csv
-let newEmployee = CareerObjParseFromString(request_string);
